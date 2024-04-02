8825122bf345dd7cea82cafcfa6c7e822849e7e8
Alex Do <alexxdo2004@gmail.com>
2024-04-02 19:24:34 +0100

    edits and making a test

---
+++ include/ConvolutionalNeuralNetwork.hpp
@@ -0,0 +1,8 @@
+#ifndef CONVOLUTIONAL_NEURAL_NETWORK_HPP
+#define CONVOLUTIONAL_NEURAL_NETWORK_HPP
+
+#include "DeepNeuralNetwork.hpp"
+#include "ConvolutionalLayer.hpp"
+#include "MaxPoolLayer.hpp"
+
+#endif
---
+++ src/TwoCategoryMNIST.cpp
@@ -0,0 +1,22 @@
+#include "ConvolutionalNeuralNetwork.hpp"
+
+void TwoCategoryMNIST(){
+    Pipeline<double> pipeline;
+
+    pipeline.pushLayer(
+        ConvolutionalLayer<double, relu<double>, relu_prime<double>>(1, 2, 28, 28, 3, 1, 0)
+    );
+
+    pipeline.pushLayer(
+        MaxPoolLayer<double>(2, 26, 26, 2, 2, 0)
+    );
+
+    pipeline.pushLayer(
+        ConvolutionalLayer<double, relu<double>, relu_prime<double>>(2, 1, 13, 13, 4, 1, 0)
+    );
+    
+    pipeline.pushLayer(
+        MaxPoolLayer<double>(2, 9, 9, 3, 3, 0)
+    );
+
+}
