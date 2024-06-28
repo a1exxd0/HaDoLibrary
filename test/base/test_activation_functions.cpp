97395d5887caf4e14bf6cf49b947a25b7e542df5
Alex Do <alexxdo2004@gmail.com>
2024-06-28 15:28:14 +0100

    test: added tests for relu-prime

--- test/base/test_activation_functions.cpp
+++ test/base/test_activation_functions.cpp
@@ -44,6 +44,45 @@ TEST(RELU, RELU_NEGATIVE) {
     EXPECT_EQ(result_d, expected_d);    
 }
 
+TEST(RELU, RELUP_ZERO) {
+    float expected_f = 0.0;
+    double expected_d = 0.0;
+
+    relu_prime<float> f;
+    relu_prime<double> d;
+    auto result_f = f(0.0);
+    auto result_d = d(0.0);
+
+    EXPECT_EQ(result_f, expected_f);
+    EXPECT_EQ(result_d, expected_d);    
+}
+
+TEST(RELU, RELUP_POSITIVE) {
+    float expected_f = 1.0;
+    double expected_d = 1.0;
+
+    relu_prime<float> f;
+    relu_prime<double> d;
+    auto result_f = f(100.0);
+    auto result_d = d(100.0);
+
+    EXPECT_EQ(result_f, expected_f);
+    EXPECT_EQ(result_d, expected_d);    
+}
+
+TEST(RELU, RELUP_NEGATIVE) {
+    float expected_f = 0.0;
+    double expected_d = 0.0;
+
+    relu_prime<float> f;
+    relu_prime<double> d;
+    auto result_f = f(-100.0);
+    auto result_d = d(-100.0);
+
+    EXPECT_EQ(result_f, expected_f);
+    EXPECT_EQ(result_d, expected_d);    
+}
+
 int main(int argc, char **argv) 
 {
     ::testing::InitGoogleTest(&argc, argv);
