f26f7d93865ba91e814464d9d679b733da0054ce
Alex Do <alexxdo2004@gmail.com>
2024-06-28 16:16:00 +0100

    test: added tests for tanh and tanh prime

--- test/base/test_activation_functions.cpp
+++ test/base/test_activation_functions.cpp
@@ -4,6 +4,7 @@
 using namespace hado;
 
 constexpr double TOLERANCE=1e-6;
+constexpr double LOOSE_TOLERANCE=1;
 
 // ---------- RELU ---------- //
 
@@ -162,6 +163,86 @@ TEST(SIGMOID, SIGMOIDP_NEGATIVE) {
     auto result_d = d(-0.4);
 
     EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);    
+}
+
+// ---------- TANH ---------- //
+
+TEST(TANH, TANH_ZERO) {
+    float expected_f = 0.0;
+    double expected_d = 0.0;
+
+    f_tanh<float> f;
+    f_tanh<double> d;
+    auto result_f = f(0.0);
+    auto result_d = d(0.0);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);   
+}
+
+TEST(TANH, TANH_POSITIVE) {
+    float expected_f = 0.761594155956;
+    double expected_d = 0.761594155956;
+
+    f_tanh<float> f;
+    f_tanh<double> d;
+    auto result_f = f(1.0);
+    auto result_d = d(1.0);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);   
+}
+
+TEST(TANH, TANH_NEGATIVE) {
+    float expected_f = -0.964027580076;
+    double expected_d = -0.964027580076;
+
+    f_tanh<float> f;
+    f_tanh<double> d;
+    auto result_f = f(-2.0);
+    auto result_d = d(-2.0);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);     
+}
+
+TEST(TANH, TANHP_ZERO) {
+    float expected_f = 1;
+    double expected_d = 1;
+
+    f_tanh_prime<float> f;
+    f_tanh_prime<double> d;
+    auto result_f = f(0.0);
+    auto result_d = d(0.0);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);   
+}
+
+TEST(TANH, TANHP_POSITIVE) {
+    float expected_f = 0.257433196703;
+    double expected_d = 0.257433196703;
+
+    f_tanh_prime<float> f;
+    f_tanh_prime<double> d;
+    auto result_f = f(1.3);
+    auto result_d = d(1.3);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);    
+}
+
+TEST(TANH, TANHP_NEGATIVE) {
+    float expected_f = 0.00134095068303;
+    double expected_d = 0.00134095068303;
+
+    f_tanh_prime<float> f;
+    f_tanh_prime<double> d;
+    auto result_f = f(-4.0);
+    auto result_d = d(-4.0);
+
+    EXPECT_NEAR(result_f, expected_f, TOLERANCE);
     EXPECT_NEAR(result_d, expected_d, TOLERANCE);    
 }
 
