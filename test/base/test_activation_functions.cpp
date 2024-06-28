143ad540a3c04275c7037bd1c38b8ebfd5d74988
Alex Do <alexxdo2004@gmail.com>
2024-06-28 15:42:05 +0100

    test: added tests for sigmoid

--- test/base/test_activation_functions.cpp
+++ test/base/test_activation_functions.cpp
@@ -3,6 +3,8 @@
 
 using namespace hado;
 
+constexpr double TOLERANCE=1e-6;
+
 // ---------- RELU ---------- //
 
 TEST(RELU, RELU_ZERO) {
@@ -78,9 +80,89 @@ TEST(RELU, RELUP_NEGATIVE) {
     relu_prime<double> d;
     auto result_f = f(-100.0);
     auto result_d = d(-100.0);
-
+ 
     EXPECT_EQ(result_f, expected_f);
-    EXPECT_EQ(result_d, expected_d);    
+    EXPECT_EQ(result_d, expected_d);   
+}
+
+// ---------- SIGMOID ---------- //
+
+TEST(SIGMOID, SIGMOID_ZERO) {
+    float expected_f = 0.5;
+    double expected_d = 0.5;
+
+    sigmoid<float> f;
+    sigmoid<double> d;
+    auto result_f = f(0.0);
+    auto result_d = d(0.0);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);   
+}
+
+TEST(SIGMOID, SIGMOID_POSITIVE) {
+    float expected_f = 0.6681877721682206;
+    double expected_d = 0.6681877721682206;
+
+    sigmoid<float> f;
+    sigmoid<double> d;
+    auto result_f = f(0.7);
+    auto result_d = d(0.7);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);   
+}
+
+TEST(SIGMOID, SIGMOID_NEGATIVE) {
+    float expected_f = 0.40131233988751425;
+    double expected_d = 0.40131233988751425;
+
+    sigmoid<float> f;
+    sigmoid<double> d;
+    auto result_f = f(-0.4);
+    auto result_d = d(-0.4);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);     
+}
+
+TEST(SIGMOID, SIGMOIDP_ZERO) {
+    float expected_f = 0.25;
+    double expected_d = 0.25;
+
+    sigmoid_prime<float> f;
+    sigmoid_prime<double> d;
+    auto result_f = f(0.0);
+    auto result_d = d(0.0);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE); 
+}
+
+TEST(SIGMOID, SIGMOIDP_POSITIVE) {
+    float expected_f = 0.22171287329309072;
+    double expected_d = 0.22171287329309072;
+
+    sigmoid_prime<float> f;
+    sigmoid_prime<double> d;
+    auto result_f = f(0.7);
+    auto result_d = d(0.7);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);   
+}
+
+TEST(SIGMOID, SIGMOIDP_NEGATIVE) {
+    float expected_f = 0.24026074574152248;
+    double expected_d = 0.24026074574152248;
+
+    sigmoid_prime<float> f;
+    sigmoid_prime<double> d;
+    auto result_f = f(-0.4);
+    auto result_d = d(-0.4);
+
+    EXPECT_FLOAT_EQ(result_f, expected_f);
+    EXPECT_NEAR(result_d, expected_d, TOLERANCE);    
 }
 
 int main(int argc, char **argv) 
