220644a960090d8241cf4b8a1926e4baaf510213
Alex Do <alexxdo2004@gmail.com>
2024-08-18 09:34:43 +0100

    test: create test for activation layer forward and backward pass

--- test/layers/test_activation_layer.cpp
+++ test/layers/test_activation_layer.cpp
@@ -2,6 +2,7 @@
 #include <HaDo/DeepNeuralNetwork>
 
 using namespace hado;
+using MatrixD = Matrix<double, Dynamic, Dynamic>;
 using Activation = ActivationLayer<relu<double>, relu_prime<double>, double>;
 
 TEST(CONSTRUCTOR, Check_Negative_Zero_Sizes) {
@@ -58,6 +59,37 @@ TEST(CONSTRUCTOR, Clone_Constructor) {
     ASSERT_EQ(result->getOutputCols(), 10);
     ASSERT_EQ(result->getOutputRows(), 10);
     ASSERT_TRUE(&layer != &(*result));
+}
+
+TEST(FORWARD_BACKWARD, RELU_FORWARD) {
+    Activation layer {1, 10, 10};
+
+    vector<MatrixD> inp = {MatrixD::Random(10,10)};
+
+    auto res = layer.forward(inp);
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            ASSERT_EQ(res[0](i,j), relu<double>{}(inp[0](i,j)));
+        }
+    }
+}
+
+TEST(FORWARD_BACKWARD, RELU_BACKWARD) {
+    Activation layer {1, 10, 12};
+
+    vector<MatrixD> inp = {MatrixD::Random(10,12)};
+    vector<MatrixD> rev = {MatrixD::Random(10, 12)};
+
+    auto in = layer.forward(inp);
+    auto res = layer.backward(rev, 0);
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 12; ++j) {
+            ASSERT_EQ(res[0](i,j),
+                relu_prime<double>{}(inp[0](i,j)) * rev[0](i,j));
+        }
+    }
 }
 
 int main(int argc, char **argv) {
